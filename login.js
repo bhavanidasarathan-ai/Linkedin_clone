70690303a081acd0cc1a905d8d213e4a20d2f2a9
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-05 11:42:24 +0530

    login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
-document
-.getElementById("loginForm")
-.addEventListener("submit",function(e){
+// LOGIN
+
+document.getElementById("loginForm").addEventListener("submit", function(e){
 
 e.preventDefault();
 
-let email=document.getElementById("loginEmail").value;
+let email=document.getElementById("loginEmail").value.trim();
 
 let password=document.getElementById("loginPassword").value;
 
@@ -20,11 +20,62 @@ alert("Login Successful");
 
 window.location.href="index.html";
 
-}
-else{
+}else{
 
 alert("Invalid Email or Password");
 
 }
+
+});
+
+
+
+// SIGNUP
+
+document.getElementById("signupForm").addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let firstName=document.getElementById("firstName").value.trim();
+
+let lastName=document.getElementById("lastName").value.trim();
+
+let email=document.getElementById("email").value.trim();
+
+let password=document.getElementById("password").value;
+
+let users=JSON.parse(localStorage.getItem("users")) || [];
+
+let exists=users.find(user=>user.email===email);
+
+if(exists){
+
+alert("Account already exists.\nPlease Login.");
+
+showLogin();
+
+return;
+
+}
+
+users.push({
+
+firstName:firstName,
+
+lastName:lastName,
+
+email:email,
+
+password:password
+
+});
+
+localStorage.setItem("users",JSON.stringify(users));
+
+alert("Registration Successful!");
+
+document.getElementById("signupForm").reset();
+
+showLogin();
 
 });
