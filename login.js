4b3c4ca07fac549c01889c404df06e0cad58bbd8
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-05 10:15:55 +0530

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,30 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit",function(e){
 
+e.preventDefault();
+
+let email=document.getElementById("loginEmail").value;
+
+let password=document.getElementById("loginPassword").value;
+
+let users=JSON.parse(localStorage.getItem("users")) || [];
+
+let user=users.find(u=>u.email===email && u.password===password);
+
+if(user){
+
+localStorage.setItem("loggedInUser",JSON.stringify(user));
+
+alert("Login Successful");
+
+window.location.href="index.html";
+
+}
+else{
+
+alert("Invalid Email or Password");
+
+}
+
+});
